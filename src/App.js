a936ee0d7d4e6a1865ff729bfcda07887d47146c
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-11-12 15:41:35 +0000

    added image

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Typography variant="h1" component="h1">Scammer Whores</Typography>
+        <img width="100" style={{borderRadius: "5px"}} src="./scammerFace.png" />
+        <Typography variant="h1" component="h1">Scammer Whores
+        </Typography>
         <Typography variant="h3" component="h3">A blockchain based Scammer Archive!</Typography>
         <Typography variant="h3" component="h3">Coming Soon on Arweave!</Typography>
       </header>
