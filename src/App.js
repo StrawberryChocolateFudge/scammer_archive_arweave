2f3efd92e3298c0818b566e418643cc9b3f0cf00
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-11-12 13:47:42 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { Typography } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Typography variant="h1" component="h1">Scammer Whores</Typography>
+        <Typography variant="h3" component="h3">A blockchain based Scammer Archive!</Typography>
+        <Typography variant="h3" component="h3">Coming Soon on Arweave!</Typography>
       </header>
     </div>
   );
