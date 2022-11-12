7db129049f7c9c6c51230766defa401894bb581c
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-11-12 18:23:26 +0000

    removed scammer face

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img width="100" style={{borderRadius: "5px"}} src="./scammerFace.png" />
         <Typography variant="h1" component="h1">Scammer Whores
         </Typography>
         <Typography variant="h3" component="h3">A blockchain based Scammer Archive!</Typography>
